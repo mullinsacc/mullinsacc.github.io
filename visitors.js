d6d2a722a5d81c01f399624c9faea309af91875e
mullinsacc <mullinsacc@gmail.com>
2022-06-27 16:21:53 -0400

    Update visitors.js

--- visitors.js
+++ visitors.js
@@ -4,7 +4,7 @@
 var user_id = 'Marceline';
 var user_email = 'marceline@thenightosphere.com';
 var user_full_name = 'Marceline The Vampire Queen';
-var user_account_id = 'The Nightosphere';
+var user_account_id = 'Wizard City';
 var user_role = 'vampire queen';
 var user_language = 'en_US';
 var user_random_data = 'Bard';
