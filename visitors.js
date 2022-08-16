edbc3e8a288380d77a3cd44386616e72bb0ce9b9
mullinsacc <mullinsacc@gmail.com>
2022-08-16 14:41:13 -0400

    Update visitors.js

--- visitors.js
+++ visitors.js
@@ -4,7 +4,7 @@
 var user_id = 'FeedbackUser';
 var user_email = 'marceline@thenightosphere.com';
 var user_full_name = 'Marceline The Vampire Queen';
-var user_account_id = 'The UpsideDown';
+var user_account_id = 'Feedback Account';
 //var user_role = 'Vampire';
 var user_language = 'en_US';
 var user_random_data = 'Bard';
@@ -14,7 +14,7 @@ var user_account_access = ['Candy Kingdom', 'Farmworld', 'Tree House', 'Land of 
 var user_account_creation_date = '07/01/2021';
 var user_current_account_id = 'Feedback Account';
 var user_current_parent_account_id = '';
-var user_acct_value = '700'
+var user_acct_value = '500'
 var user_is_paying = 'true'
 
 /*
