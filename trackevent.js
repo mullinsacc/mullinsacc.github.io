d414fd262c0a1e2db4add258929008281b7cda81
mullinsacc <mullinsacc@gmail.com>
2022-09-21 13:24:07 -0400

    Update trackevent.js

--- trackevent.js
+++ trackevent.js
@@ -1,6 +1,5 @@
 pendo.track("Course Name", {
-  registered: "true",
-  timestamp: 2021-08-01T15:04:05.999-5:00
+  registered: "true"
 });
 pendo.track("Search Term Result", {
   term: "looking for stuff",
