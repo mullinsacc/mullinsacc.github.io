513f31a7a1da6c8947497813eb8126f606f1ea4e
mullinsacc <anna.mullins@pendo.io>
2025-01-10 09:54:02 -0500

    Update trackevent.js

--- trackevent.js
+++ trackevent.js
@@ -1,4 +1,4 @@
 pendo.track("patient_conditions", {
-  patient_id: "santa_claus",
-  conditions: "175.023 checked 2024; G32.3 checked 2022; D68.9 checked 2024"
+  patient_id: "rudolph",
+  conditions: "68.01 checked 2022; R54.3 checked 2024; 368.4 checked 2024"
 });
